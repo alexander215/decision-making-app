a2e42411811b24efdca61206322af8497dba9b7e
alexander215 <alexander.hoskinson@colorado.edu>
2019-10-02 00:04:49 -0600

    Create routes folder

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import { Route } from 'react-router-dom';
 import IntroSection from '../IntroSection';
-
+import * as ROUTES from '../../constants/routes';
 
 function App() {
   return (
     <div>
-      <IntroSection />
+      <Route exact path={ROUTES.LANDING_PAGE} component={IntroSection} />
     </div>
   );
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,2 @@
+export const LANDING_PAGE = '/';
+export const DECISIONS_PAGE = '/decisions';
