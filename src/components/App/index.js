dd77333490e38ebdf5958aba4e4d0fce6f0e3c58
alexander215 <alexander.hoskinson@colorado.edu>
2019-11-05 15:03:33 -0800

    Add 404 error message

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import './style.css';
-import { Route } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import IntroSection from '../IntroSection';
 import DecisionList from '../DecisionList';
 import RandomNumber from '../RandomNumber';
 import * as ROUTES from '../../constants/routes';
 
+
+const Error404Message = () => {
+  return (
+    <div>
+      <h1>Bad decision, that page doesn't exist.</h1>
+      <Link to={ROUTES.LANDING_PAGE}>Let's head back to try again.</Link>
+    </div>
+  )
+}
 function App() {
   return (
     <div>
-      <Route exact path={ROUTES.LANDING_PAGE} component={IntroSection} />
-      <Route exact path={ROUTES.DECISIONS_LIST} component={DecisionList} />
-      <Route exact path={ROUTES.RANDOM_NUMBER} component={RandomNumber} />
+      <Switch>
+        <Route exact path={ROUTES.LANDING_PAGE} component={IntroSection} />
+        <Route exact path={ROUTES.DECISIONS_LIST} component={DecisionList} />
+        <Route exact path={ROUTES.RANDOM_NUMBER} component={RandomNumber} />
+
+        <Route component={ Error404Message } />
+      </Switch>
     </div>
   );
 }
