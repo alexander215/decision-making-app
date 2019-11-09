e8f5cad70ddb7adc7970f3ad371f81186e1895c3
alexander215 <alexander.hoskinson@colorado.edu>
2019-11-08 16:20:35 -0800

    Basic selection hook for rock paper and scissors

--- src/components/RockPaperScissors/index.js
+++ src/components/RockPaperScissors/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 const RockPaperScissors = () => {
+    const [currentChoice, setCurrentChoice] = useState('Rock');
+    
+
     return (
         <div>
             <h1>Rock, Paper, Scissors</h1>
@@ -8,7 +11,17 @@ const RockPaperScissors = () => {
             Rock breaks scissors, scissors cut paper, paper covers rock. It's a vicious circle.
             No one knows why a rock gives up when it's covered by a piece of paper, but that's neither here nor there.
             <hr/>
+            <div cursor='pointer' value='Rock' className={(currentChoice == 'Rock') ? 'selected' : ''} onClick={e => setCurrentChoice('Rock')}>
 
+                Rock.
+            </div>
+            <div value='Rock' className={(currentChoice == 'Paper') ? 'selected' : ''} onClick={e => setCurrentChoice('Paper')}>
+                Paper.
+            </div>
+            <div value='Rock' className={(currentChoice == 'Scissors') ? 'selected' : ''}onClick={e => setCurrentChoice('Scissors')}>
+                Scissors.
+            </div>
+            
 
         </div>
     )
