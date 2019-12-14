d96227667a94e985bbca815d37f02f9dad3c512c
alexander215 <alexander.hoskinson@colorado.edu>
2019-12-13 19:09:34 -0700

    Flip a coin has a flip status hook that changes visibility depending on whether or not the coin has been flipped

--- src/components/FlipACoin/index.js
+++ src/components/FlipACoin/index.js
@@ -6,6 +6,7 @@ const FlipACoin = () => {
     const [preferredDecision, setPreferredDecision] = useState('');
     const [otherDecision, setOtherDecision] = useState('');
     const [resultStatus, setResultStatus] = useState('');
+    const [flipStatus, setFlipStatus] = useState('notFlipped');
 
     // This will decide the result of the coin flip: heads or tails
     const coinDecision = () => {
@@ -30,7 +31,12 @@ const FlipACoin = () => {
         } else {
             let flippedCoin = coinDecision();
             flipWinner(flippedCoin);
+            setFlipStatus('flipped')
         }
+    }
+
+    const resetTheGame = () => {
+        setFlipStatus('notFlipped');
     }
 
     return (
@@ -40,26 +46,39 @@ const FlipACoin = () => {
             <br/>
             All you have to do is enter the decision you'd like, and choose whether it happens on heads or tails. Let fate do the rest.
             <hr/>
-            <div>
-                Your desired result. In other words, if the coin agrees, you will:
-                <br/>
-                <input type='text' name='desired-decision' placeholder='Enter result' onChange={e => setPreferredDecision(e.target.value)}></input>
-                <br/>
-                The other result. If the coin doesn't agree, you will:
-                <br/>
-                <input type='text' name='other-decision' placeholder='Enter result' onChange={e => setOtherDecision(e.target.value)}></input>
+
+            {/* This is the selection criteria. It will be hidden once the coin is flipped, until a new game is started. */}
+            <div className={(flipStatus === 'notFlipped') ? 'visible' : 'hidden'} >
+                <div>
+                    Your desired result. In other words, if the coin agrees, you will:
+                    <br/>
+                    <input type='text' name='desired-decision' placeholder='Enter result' onChange={e => setPreferredDecision(e.target.value)}></input>
+                    <br/>
+                    The other result. If the coin doesn't agree, you will:
+                    <br/>
+                    <input type='text' name='other-decision' placeholder='Enter result' onChange={e => setOtherDecision(e.target.value)}></input>
+                </div>
+
+                {/* className={(props.circleSelected == number) ? 'selected' : ''} */}
+                {/* visibility: visible; */}
+
+                <div>(Click choice to toggle.)</div>
+                <div value='Heads' className={(currentChoice === 'Heads') ? 'selected' : ''} onClick={e => setCurrentChoice('Heads')}>Heads</div>
+                <div value='Tails' className={(currentChoice === 'Tails') ? 'selected' : ''} onClick={e => setCurrentChoice('Tails')}>Tails</div>
+                <button onClick={e => flipTheCoin()}>Flip the coin!</button>
             </div>
 
-            <div>(Click choice to toggle.)</div>
-            <div value='Heads' className={(currentChoice === 'Heads') ? 'selected' : ''} onClick={e => setCurrentChoice('Heads')}>Heads</div>
-            <div value='Tails' className={(currentChoice === 'Tails') ? 'selected' : ''} onClick={e => setCurrentChoice('Tails')}>Tails</div>
-            <button onClick={e => flipTheCoin()}>Flip the coin!</button>
-            <h2>Flip results:</h2>
-            Your choice is: {currentChoice}
-            <br/>
-            The result is: {flipResult}
-            <br/>
-            {resultStatus}
+            {/* This is the result. This will be hidden until the coin is flipped. */}
+            <div className={(flipStatus === 'notFlipped') ? 'hidden' : 'visible'}>
+                <h2>Flip results:</h2>
+                Your choice is: {currentChoice}
+                <br/>
+                The result is: {flipResult}
+                <br/>
+                {resultStatus}
+                <button onClick={e => resetTheGame()}>Set up another flip.</button>
+            </div>
+
         </div>
     )
 }
