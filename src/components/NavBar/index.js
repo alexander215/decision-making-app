5e8f514aba0015df952a5bf43167df8f5b4b53d0
alexander215 <alexander.hoskinson@colorado.edu>
2020-07-10 22:04:05 -0700

    Add home link to navbar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
 
 const index = () => {
   return (
     <div>
-      Navbar
+      <Link to={ROUTES.LANDING_PAGE}>Home</Link>
     </div>
   )
 }
